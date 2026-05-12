172b3ba28d7b1bc3a01ce51e2715aa7d95dafc63
Jordan Coeyman <jcoeyman@cloudflare.com>
2026-05-12 12:10:13 -0400

    Skip container migration generation

--- alchemy.run.ts
+++ alchemy.run.ts
@@ -51,6 +51,7 @@ const runnerName = process.env.CLOUDBOX_RUNNER_NAME || (isProd ? "cloudbox-runne
 const CLOUDBOX_RUNNER = await Container(runnerResourceId, {
   name: runnerName,
   className: "CloudboxRunner",
+  scriptName: "__cloudbox-container-app-only__",
   build: {
     context: "./runner",
     dockerfile: "Dockerfile",
